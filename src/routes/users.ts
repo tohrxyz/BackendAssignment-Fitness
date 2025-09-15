84ec0b7deeac5c610cea0f0c177684da66866e04
tohrxyz <103599310+tohrxyz@users.noreply.github.com>
2025-09-15 15:30:29 +0200

    feat: add single dynamic user route

---
+++ src/routes/users.ts
@@ -0,0 +1,34 @@
+import { Router, Request, Response, NextFunction } from 'express'
+import { models } from '../db'
+import { getReturnError } from '../utils/routeHelpers'
+import { ERROR_400_UKNOWN } from '../constants/statusCodeMessages'
+
+const router = Router()
+
+const {
+  User
+} = models
+
+router.get('/:id', async (_req: Request, _res: Response, _next: NextFunction): Promise<any> => {
+  const userId = _req.params.id
+  if (!userId || userId === '' || userId === ' ') {
+    getReturnError(_res, "Invalid or missing user id")
+  }
+
+  try {
+    const user = await User.findOne({ where: { id: userId }})
+    if (!user) {
+      throw new Error("Cannot get user")
+    }
+
+    return _res.json({
+      data: user,
+      message: 'User object'
+    })
+  } catch(e) {
+    console.error(`[ERROR] /users/${userId} : ${e})`)
+    getReturnError(_res, ERROR_400_UKNOWN)
+  }
+})
+
+export default router
