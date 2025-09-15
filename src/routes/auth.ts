2a3d224f78e1af17ef7aa1d1b707dba91783aabe
tohrxyz <103599310+tohrxyz@users.noreply.github.com>
2025-09-15 17:30:44 +0200

    feat: add auth login route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,7 +4,7 @@ import { getReturnError } from '../utils/routeHelpers'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { getErrorMsgMissingParams } from '../utils/validation'
-import { ERROR_400_UKNOWN } from '../constants/statusCodeMessages'
+import { ERROR_401_INVALID_CREDENTIALS, ERROR_500_UKNOWN, SUCCESS_200_LOGIN_SUCCESSFULL, SUCCESS_201_REGISTRATION_SUCCESSFULL } from '../constants/statusCodeMessages'
 
 const router = Router()
 
@@ -44,7 +44,7 @@ router.post('/register', async (_req: Request, _res: Response, _next: NextFuncti
     )
 
     return _res.status(201).json({
-      message: 'User registered successfully',
+      message: SUCCESS_201_REGISTRATION_SUCCESSFULL,
       data: {
         token,
         user: JSON.stringify(newUser)
@@ -53,7 +53,51 @@ router.post('/register', async (_req: Request, _res: Response, _next: NextFuncti
   } catch(e) {
     console.error(`[ERROR] /auth/register: ${e}`)
     return _res.status(500).json({
-      error: ERROR_400_UKNOWN
+      error: ERROR_500_UKNOWN
+    })
+  }
+})
+
+router.post('/login', async(_req: Request, _res: Response, _next: NextFunction): Promise<any> => {
+  const invalidParamsOrNull = getErrorMsgMissingParams(_req.body)
+  if (invalidParamsOrNull !== null) {
+    return getReturnError(_res, invalidParamsOrNull)
+  }
+
+  const { email, password } = _req.body
+
+  try {
+    const user = await User.findOne({ where: { email }})
+    if (!user) {
+      return _res.status(401).json({
+        error: ERROR_401_INVALID_CREDENTIALS // attacker shouldnt be able to enumerate existing emails
+      })
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordCorrect) {
+      return _res.status(401).json({
+        error: ERROR_401_INVALID_CREDENTIALS
+      })
+    }
+
+    const token = jwt.sign(
+      { id: user.id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: '7d'}
+    )
+
+    return _res.status(200).json({
+      message: SUCCESS_200_LOGIN_SUCCESSFULL,
+      data: {
+        token
+      }
+    })
+  } catch(e) {
+    console.error(`[ERROR] /auth/login : ${e}`)
+    return _res.status(500).json({
+      error: ERROR_500_UKNOWN
     })
   }
 })
