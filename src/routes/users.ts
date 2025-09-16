034e1d6a7d968b79feafc7d40fba33c6b63dfd44
tohrxyz <103599310+tohrxyz@users.noreply.github.com>
2025-09-16 14:06:24 +0200

    feat: protect user api routes, allow admin to get user detail, users, edit user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,6 +2,8 @@ import { Router, Request, Response, NextFunction } from 'express'
 import { models } from '../db'
 import { getReturnError } from '../utils/routeHelpers'
 import { ERROR_500_UKNOWN } from '../constants/statusCodeMessages'
+import { adminMiddleware, authMiddleware } from './auth'
+import { getErrorMsgMissingParams } from '../utils/validation'
 
 const router = Router()
 
@@ -9,7 +11,7 @@ const {
   User
 } = models
 
-router.get('/:id', async (_req: Request, _res: Response, _next: NextFunction): Promise<any> => {
+router.get('/:id', [authMiddleware, adminMiddleware], async (_req: Request, _res: Response, _next: NextFunction): Promise<any> => {
   const userId = _req.params.id
   if (!userId || userId === '' || userId === ' ') {
     getReturnError(_res, "Invalid or missing user id")
@@ -28,6 +30,76 @@ router.get('/:id', async (_req: Request, _res: Response, _next: NextFunction): P
   } catch(e) {
     console.error(`[ERROR] /users/${userId} : ${e})`)
     getReturnError(_res, ERROR_500_UKNOWN)
+  }
+})
+
+// all users
+router.get('/', [authMiddleware, adminMiddleware], async (_req: Request, _res: Response, _next: NextFunction): Promise<any> => {
+
+  try {
+    const users = await User.findAll()
+    if (!users) {
+      throw new Error("Cannot get user")
+    }
+
+    return _res.json({
+      data: users,
+      message: 'User objects array'
+    })
+  } catch(e) {
+    console.error(`[ERROR] /users/ : ${e})`)
+    getReturnError(_res, ERROR_500_UKNOWN)
+  }
+})
+
+router.put('/:id/', [authMiddleware, adminMiddleware], async (_req: Request, res: Response, _next: NextFunction): Promise<any> => {
+  const { id } = _req.params
+  const { name, surname, nickName, age, role } = _req.body
+
+  const userData = {
+    name, surname, nickName, age, role
+  }
+
+  const dataToUpdate = Object.fromEntries(
+    Object.entries(userData).filter(([_, value]) => value && value !== " ")
+  )
+
+  // this checks if specified have value
+  const invalidParamsOrNull = getErrorMsgMissingParams(dataToUpdate)
+  if (invalidParamsOrNull) {
+    getReturnError(res, invalidParamsOrNull)
+    return
+  }
+
+  // this checks if there are properties to update at all
+  if (Object.entries(dataToUpdate).length === 0) {
+    getReturnError(res, "Nothing to update")
+    return
+  }
+
+  try {
+    void await User.update(dataToUpdate, { where: { id }})
+    const updatedUser = await User.findOne({ where: { id }})
+
+    res.status(200).json({
+      message: "User updated successfully",
+      data: {
+        user: {
+          name: updatedUser.name,
+          surname: updatedUser.surname,
+          nickName: updatedUser.nickName,
+          age: updatedUser.age,
+          role: updatedUser.role
+        }
+      }
+    })
+    return
+  } catch(e) {
+    console.error(`[ERROR] /user/id (update): ${e}`)
+    res.status(500).json({
+      error: ERROR_500_UKNOWN
+    })
+    return
   }
 })
 
